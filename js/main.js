5cb431c4552db956fb2e4baef2bca37f592e5b87
Konrad <konrad.wojtylo.9@gmail.com>
2022-12-30 20:55:36 +0100

    js optimization

--- js/main.js
+++ js/main.js
@@ -1,81 +1,102 @@
-const body = document.querySelector('body')
-const navMobile = document.querySelector('.nav-mobile')
-const navBtn = document.querySelector('.hamburger')
-const allNavItems = document.querySelectorAll('.nav__link')
-const actualYear = document.querySelector('.footer__year')
-const newsletterBtn = document.querySelector('.newsletter__form-btn')
-const newsletterInput = document.querySelector('.newsletter__form-input')
-const contactUsername = document.querySelector('#name')
-const contactUserEmail = document.querySelector('#email')
-const contactUserMsg = document.querySelector('#msg')
-const contactErrorMsg = document.querySelector('.contact__form-error')
-const contactBtn = document.querySelector('.contact__form-btn')
-const emailCheck = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/i
+let BODY
+let NAV_MOBILE
+let NAV_BTN
+let ALL_NAV_ITEMS
+let ACTUAL_YEAR
+let NEWSLETTER_BTN
+let NEWSLETTER_INPUT
+let CONTACT_USERNAME
+let CONTACT_USER_EMAIL
+let CONTACT_USER_MSG
+let CONTACT_ERROR_MSG
+let CONTACT_BTN
+let EMAIL_CHECK
+let INPUTS_ARR
 
-const inputsArr = [contactUsername, contactUserEmail, contactUserMsg]
+const main = () => {
+    prepareDOMElements()
+    prepareDOMEvents()
+}
+
+const prepareDOMElements = () => {
+    BODY = document.querySelector('body')
+    NAV_MOBILE = document.querySelector('.nav-mobile')
+    NAV_BTN = document.querySelector('.hamburger')
+    ALL_NAV_ITEMS = document.querySelectorAll('.nav__link')
+    NEWSLETTER_BTN = document.querySelector('.newsletter__form-btn')
+    NEWSLETTER_INPUT = document.querySelector('.newsletter__form-input')
+    CONTACT_USERNAME = document.querySelector('#name')
+    CONTACT_USER_EMAIL = document.querySelector('#email')
+    CONTACT_USER_MSG = document.querySelector('#msg')
+    CONTACT_ERROR_MSG = document.querySelector('.contact__form-error')
+    CONTACT_BTN = document.querySelector('.contact__form-btn')
+    ACTUAL_YEAR = document.querySelector('.footer__year')
+    EMAIL_CHECK = /[a-z0-9]+@[a-z]+\.[a-z]{2,3}/i
+    INPUTS_ARR = [CONTACT_USERNAME, CONTACT_USER_EMAIL, CONTACT_USER_MSG]
+}
+
+const prepareDOMEvents = () => {
+	currentYear()
+	NEWSLETTER_BTN.addEventListener('click', newsletterCheck)
+	CONTACT_BTN.addEventListener('click', contactBtnAction)
+	NAV_BTN.addEventListener('click', handleNav)
+}
 
 const scrollBlockNavBtn = () => {
-    if(body.classList.contains('scroll-block')) {
-        body.classList.remove('scroll-block')
-    } else {
-        body.classList.add('scroll-block')
-    }
+	if (BODY.classList.contains('scroll-block')) {
+		BODY.classList.remove('scroll-block')
+	} else {
+		BODY.classList.add('scroll-block')
+	}
 }
 
 const handleNav = () => {
-    navMobile.classList.toggle('nav-mobile--active')
-    navBtn.classList.toggle('is-active')
+	NAV_MOBILE.classList.toggle('nav-mobile--active')
+	NAV_BTN.classList.toggle('is-active')
 
-    allNavItems.forEach(item => {
-        item.addEventListener('click', () => {
-            navMobile.classList.remove('nav-mobile--active')
-            navBtn.classList.remove('is-active')
-            body.classList.remove('scroll-block')
-        })
-    })
+	ALL_NAV_ITEMS.forEach(item => {
+		item.addEventListener('click', () => {
+			NAV_MOBILE.classList.remove('nav-mobile--active')
+			NAV_BTN.classList.remove('is-active')
+			BODY.classList.remove('scroll-block')
+		})
+	})
 
-    scrollBlockNavBtn()
+	scrollBlockNavBtn()
 }
 
 const newsletterCheck = () => {
+	if (EMAIL_CHECK.test(NEWSLETTER_INPUT.value)) {
+		NEWSLETTER_BTN.textContent = 'Got it!'
+		NEWSLETTER_INPUT.value = ''
+	} else {
+		NEWSLETTER_BTN.textContent = 'Incorrect..'
+	}
 
-    if(emailCheck.test(newsletterInput.value)) {
-        newsletterBtn.textContent = 'Got it!'
-        newsletterInput.value = ''
-    } else {
-        newsletterBtn.textContent = 'Incorrect..'
-    }
-
-    setTimeout(() => {
-        newsletterBtn.textContent = 'Subscribe!'
-    }, 2500)
+	setTimeout(() => {
+		NEWSLETTER_BTN.textContent = 'Subscribe!'
+	}, 2500)
 }
 
 const contactBtnAction = () => {
+	if (CONTACT_USERNAME.value !== '' && CONTACT_USER_MSG.value !== '' && EMAIL_CHECK.test(CONTACT_USER_EMAIL.value)) {
+		;(CONTACT_BTN.textContent = 'Message sent!'),
+			INPUTS_ARR.forEach(el => {
+				el.value = ''
+			})
+		CONTACT_ERROR_MSG.classList.add('error-toggle')
 
-    if(contactUsername.value !== '' && contactUserMsg.value !== '' && emailCheck.test(contactUserEmail.value)) {
-        contactBtn.textContent = 'Message sent!',
-        inputsArr.forEach(el => {
-            el.value = ''
-        })
-        contactErrorMsg.classList.add('error-toggle')
-
-        setTimeout(() => {
-            contactBtn.textContent = 'Send'
-        }, 2500)
-
-    } else {
-        contactErrorMsg.classList.remove('error-toggle')
-    }
+		setTimeout(() => {
+			CONTACT_BTN.textContent = 'Send'
+		}, 2500)
+	} else {
+		CONTACT_ERROR_MSG.classList.remove('error-toggle')
+	}
 }
 
 const currentYear = () => {
-    const newDate = new Date().getFullYear()
-    actualYear.textContent = newDate
+	const newDate = new Date().getFullYear()
+	ACTUAL_YEAR.textContent = newDate
 }
 
-currentYear()
-
-newsletterBtn.addEventListener('click', newsletterCheck)
-contactBtn.addEventListener('click', contactBtnAction)
-navBtn.addEventListener('click', handleNav)
+document.addEventListener('DOMContentLoaded', main)
